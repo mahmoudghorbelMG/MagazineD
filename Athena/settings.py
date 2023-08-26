d69153261b199fa8042b5c9cda9a3a29829fa0c7
firasbarkia <92690477+firasbarkia@users.noreply.github.com>
2023-08-26 18:57:21 +0100

    Update settings.py

--- Athena/settings.py
+++ Athena/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["57.128.168.6","c-athena.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
