0d336a7521c32191a40cac194477c503a84576b6
Mahmoud Ghorbel <mahmoud.ghorbel.p@citeo.com>
2023-09-02 14:17:40 +0200

    cancel relative path

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from .Athena import settings
+from Athena import settings
 
 def main():
     """Run administrative tasks."""
